45e1aca4e775621b9ba550b4334619b7e0c2cd9b
Omryno1 <Omryno1@gmail.com>
2017-02-25 10:43:08 +0200

    Initial Commit

---
+++ WildLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WildLife
+//
+//  Created by Omry Dabush on 25/02/2017.
+//  Copyright © 2017 Omry Dabush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
